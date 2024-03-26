36251d6a0129de5516e32b58668b9e936fad4a23
aaisharajput <sapnadevi@aarkglobalinc.com>
2024-03-26 15:37:46 +0530

    home and layout page

--- app/layoutDemo.js
+++ app/layoutDemo.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./globalsDemo.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+
+const HomePage = () => {
+  return (
+    <div className='demo d-flex justify-content-center align-items-center bg-dark'>
+      home page
+    </div>
+  )
+}
+
+export default HomePage
